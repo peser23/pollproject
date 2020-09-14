6e6476a18e85caa957cd05916e0c6513d910e310
Praveen Peser <prav@gmail.com>
2020-09-13 22:20:26 -0400

    using generic views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 
 
@@ -8,6 +9,15 @@ def index(request):
     latest_questions_list = Question.objects.order_by('-pub_date')[:5:]
     output = '\n'.join(q.question_text for q in latest_questions_list)
     return render(request, 'polls/index.html', {'latest_questions_list':latest_questions_list})
+
+
+class IndexView(generic.ListView):
+    model = Question
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5:]
 
 
 def detail(request, question_id):
@@ -18,9 +28,21 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', context)
 
 
+class DetailView(generic.DetailView):
+    model = Question
+    context_object_name = 'question'
+    template_name = 'polls/detail.html'
+
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    context_object_name = 'question'
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
